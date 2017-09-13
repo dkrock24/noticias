38446c0274a23391956a2ef110ba535bee3f3ff8
jose.lopez04 <jose.lopez04@telusinternational.com>
2017-09-13 08:27:23 -0600

    merge jose.lopez cambios de aprobacion

--- application/views/backend/perfil/VchangePassword.php
+++ application/views/backend/perfil/VchangePassword.php
@@ -1,26 +1,5 @@
 
 
- <!-- END PRELOADER -->
-    <a href="#" class="scrollup"><i class="fa fa-angle-up"></i></a> 
-    
-    <script src="../../../assets/plugins/jquery/jquery-migrate-1.2.1.min.js"></script>
-    <script src="../../../assets/plugins/jquery-ui/jquery-ui-1.11.2.min.js"></script>
-    <script src="../../../assets/plugins/gsap/main-gsap.min.js"></script>
-    <script src="../../../assets/plugins/bootstrap/js/bootstrap.min.js"></script>
-    <script src="../../../assets/plugins/jquery-cookies/jquery.cookies.min.js"></script> <!-- Jquery Cookies, for theme -->
-    <script src="../../../assets/plugins/jquery-block-ui/jquery.blockUI.min.js"></script> <!-- simulate synchronous behavior when using AJAX -->
-    <script src="../../../assets/plugins/translate/jqueryTranslator.min.js"></script> <!-- Translate Plugin with JSON data -->
-    <script src="../../../assets/js/sidebar_hover.js"></script> <!-- Sidebar on Hover -->
-    <script src="../../../assets/js/plugins.js"></script> <!-- Main Plugin Initialization Script -->
-    <script src="../../../assets/js/widgets/notes.js"></script> <!-- Notes Widget -->
-    <script src="../../../assets/js/quickview.js"></script> <!-- Chat Script -->
-    <script src="../../../assets/js/pages/search.js"></script> <!-- Search Script -->
-    <!-- BEGIN PAGE SCRIPT -->
-    <script src="../../../assets/plugins/datatables/jquery.dataTables.min.js"></script> <!-- Tables Filtering, Sorting & Editing -->
-    <script src="../../../assets/js/pages/table_dynamic.js"></script>
-    <!-- BEGIN PAGE SCRIPT -->
-    <link href="../../../assets/plugins/input-text/style.min.css" rel="stylesheet">
-
     <script>
     	$(document).ready(function(){
     		$(".msg1").hide();
@@ -159,6 +138,68 @@
     background-color: #9AC835;
   }
 </style>
+
+
+<div class="row">
+      <div class="col-md-12">
+            <div class="card card-primary">
+                  <div class="card-header">
+                        <div class="header-block">
+                              <div class="container">
+                                    <div class="row abc">
+                                          <div class="col-md-8 global_text">Cambiar Contraseña</div>
+                                          
+                                    </div>
+                              </div>
+                        </div>
+                  </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="table-flip-scroll">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card-block">
+                <div class="row">
+                    <div class="col-md-6">   
+                        <input class="form-control" type="password" id="password1" />                     
+                    </div>
+                    <div class="col-md-6">   
+                    <input class="form-control" type="password" id="password2" required />                     
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-md-6">  
+                    <span class="">Digite La Nueva Contraseña</span>                      
+                    </div>
+                    <div class="col-md-6">   
+                    <span class="">Vuelva a  Digitar La Nueva Contraseña</span>
+                    </div>
+                </div>
+                    <br>
+                <div class="row">
+                    <div class="col-md-12">  
+                        <div class="demo">
+                        Nivel de Seguridad <span class='nivel'></span>
+                            <div class="progress progress-bar-large">
+                                <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" >
+                                    <span class="sr-only">60% Complete (warning)</span>
+                                </div>
+                            </div>
+                        </div>        
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
 <input type="hidden" value="<?php echo $_SESSION['idUser']; ?>" name="idusuario" id="id_usuario">
 <div class="tab-content">
     <div class="tab-pane fade active in" id="tab1_1">
@@ -167,13 +208,7 @@
     	
     	<div class="col-md-12">
     	<div class="panel bg-dark">
-                <div class="panel-header">
-                  <h3>Importante! - <strong>Leer para activar el usuario</strong></h3>
-                  <div class="control-btn">
-                    <a href="#" class="panel-toggle"><i class="fa fa-angle-down"></i></a>
-                    <a href="#" class="panel-close"><i class="icon-trash"></i></a>
-                  </div>
-                </div>
+
                 <div class="panel-content" style="display: none;">
                 	<p>
     					Se debe cambiar la contraseña del sistema para el usuario......<strong><?php echo $_SESSION["data"][3]; ?></strong><br>
@@ -198,39 +233,10 @@
         	<div class="msg3" ><h3>No se Cambio La Contraseña. Problemas de Acceso.. </h3></div>
         </div>
     </div>
-    <div class="row">
-        <div class="col-md-12">
-          <div class="row line">
-                <div class="col-md-6">
-                  <span class="input input--hoshi">
-                      <input class="input__field input__field--hoshi" type="password" id="password1" />
-                      <label class="input__label input__label--hoshi input__label--hoshi-color-1" for="input-4">
-                      <span class="input__label-content">Digite La Nueva Contraseña</span>
-                      </label>
-                  </span>
-                </div>
-                <div class="col-md-6">
-                  <span class="input input--hoshi">
-                      <input class="input__field input__field--hoshi" type="password" id="password2" required />
-                      <label class="input__label input__label--hoshi input__label--hoshi-color-1" for="input-4">
-                      <span class="input__label-content">Vuelva a  Digitar La Nueva Contraseña</span>
-                      </label>
-                  </span>
-                </div>
-          </div> 
-        </div>
-    </div>
+
     <div class="row">
     <div class="col-md-10">
-    	<div class="demo">
-    	Nivel de Seguridad <span class='nivel'></span>
-    		<div class="progress progress-bar-large">
-                <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" >
-                <span class="sr-only">60% Complete (warning)</span>
-                
-                </div>
-            </div>
-    	</div>
+    	
     </div>
         <div class="col-md-2">
         	<input type="button" id="cambiar_password" class="btn btn-primary" value="Guardar">
